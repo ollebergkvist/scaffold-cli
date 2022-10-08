5c000c6744b8cfc4317a65bb1d7f358431a8110b
Olle Bergkvist <hello@Ollebergkvist.com>
2022-10-08 11:32:36 -0600

    style: lint

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,9 +1,9 @@
 /** @type {import("@types/prettier").Options} */
 
 module.exports = {
-  "tabWidth": 2,
-  "singleQuote": true,
-  "printWidth": 100,
-  "semi": false,
-  "trailingComma": "all"
+  tabWidth: 2,
+  singleQuote: true,
+  printWidth: 100,
+  semi: false,
+  trailingComma: 'all',
 }
