abbfd97174a1f0cadc7a7270d4e171b262f9964a
Olle Bergkvist <hello@Ollebergkvist.com>
2022-10-10 14:32:28 -0600

    build: add micromatch to ignore jest and cypress config ts files

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,4 +1,9 @@
+import micromatch from 'micromatch'
+
 module.exports = {
-  '*.{js,jsx,ts,tsx,astro}': ['eslint'],
+  '*.{js,jsx,ts,tsx,astro}': (files) => {
+    const match = micromatch.not(files, 'jest.config.ts', 'cypress.config.ts')
+    return `eslint ${match.join(' ')}`
+  },
   '*.json': ['prettier --write'],
 }
