4217b2e9701f22a596858c789511e473af9857ea
Olle Bergkvist <hello@Ollebergkvist.com>
2022-10-10 17:42:32 -0600

    build: remove plugin:@typescript-eslint/strict

--- configs/eslint/plugins/eslint-tsx.cjs
+++ configs/eslint/plugins/eslint-tsx.cjs
@@ -11,7 +11,6 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'plugin:react/jsx-runtime',
-        'plugin:@typescript-eslint/strict',
 
         'prettier',
       ],
