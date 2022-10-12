22212b8a415fd7f18142717a2be991b088769eac
Olle Bergkvist <hello@Ollebergkvist.com>
2022-10-12 10:53:53 -0600

    feat: add barrelsby description

--- src/helpers/options.ts
+++ src/helpers/options.ts
@@ -1,5 +1,6 @@
 export const Options = {
   astro: 'installs common tools, typescript and eslint astro specific configs',
+  barrelsby: 'installs barrelsby',
   commitLint: 'installs commitlint, adds husky commit-msg hook',
   cypress: 'installs cypress, adds .cypress.cjs',
   eslint: 'installs eslint, adds .eslintrc.cjs',
