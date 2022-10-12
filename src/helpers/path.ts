ec68ed1e59195969335d25d8fbf23525edf1e677
Olle Bergkvist <hello@Ollebergkvist.com>
2022-10-12 11:36:22 -0600

    style: fix lint errors

--- src/helpers/path.ts
+++ src/helpers/path.ts
@@ -1,7 +1,10 @@
 import path from 'path'
 import { fileURLToPath } from 'url'
 
+// eslint-disable-next-line no-underscore-dangle, @typescript-eslint/naming-convention
 const __filename = fileURLToPath(import.meta.url)
+
+// eslint-disable-next-line @typescript-eslint/naming-convention, no-underscore-dangle
 const __dirname = path.dirname(__filename)
 
 export const resolvePath = (filePath: string) => {
